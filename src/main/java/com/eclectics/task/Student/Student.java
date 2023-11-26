7681c3cbf929b4c82be76968fbc0b2f5734a1578
stephen munyao <stevemighty2030@gmail.com>
2023-11-26 17:23:14 +0300

    modified the Entity class

--- src/main/java/com/eclectics/task/Student/Student.java
+++ src/main/java/com/eclectics/task/Student/Student.java
@@ -1,4 +1,22 @@
 package com.eclectics.task.Student;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+
 }
